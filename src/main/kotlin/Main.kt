a0de6692e84711b5af8d759909318fd47d8515d0
박진성 <mac9692@nate.com>
2026-01-26 23:10:04 +0900

    feat: 객체 식(익명클래스)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import clazz.PrivatePerson
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 fun main() {
-    val name = "Kotlin"
+    var name = "Kotlin"
     //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
     // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
     println("Hello, $name!")
@@ -30,4 +30,13 @@ fun main() {
 
     val clazz = ClassByFactory.create(arrayOf("박", "진", "성")) ?: return
     println(clazz.name)
+
+    val anonymousClass = object {
+        fun change() {
+            name = "변경"
+        }
+    }
+
+    anonymousClass.change()
+    println(name)
 }
